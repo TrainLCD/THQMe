34a7a42a1e31d2dbe42f96cb889feb5f0a71e695
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-08-15 01:48:29 +0900

    つるぺたJSON

--- app.config.js
+++ app.config.js
@@ -1,45 +1,43 @@
 export default {
-  expo: {
-    name: "thqme",
-    slug: "thqme",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "thqme",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "me.tinykitten.trainlcd.thq",
-      appleTeamId: process.env.EXPO_APPLE_TEAM_ID,
+  name: "thqme",
+  slug: "thqme",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "thqme",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "me.tinykitten.trainlcd.thq",
+    appleTeamId: process.env.EXPO_APPLE_TEAM_ID,
+  },
+  android: {
+    package: "me.tinykitten.trainlcd.thq",
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    android: {
-      package: "me.tinykitten.trainlcd.thq",
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
+    edgeToEdgeEnabled: true,
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
         backgroundColor: "#ffffff",
       },
-      edgeToEdgeEnabled: true,
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
-        },
-      ],
     ],
-    experiments: {
-      typedRoutes: true,
-    },
+  ],
+  experiments: {
+    typedRoutes: true,
   },
 };
