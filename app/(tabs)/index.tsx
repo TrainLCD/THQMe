cbc5852deb73812c122d75d3a448fc4b132095db
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-08-15 20:27:04 +0900

    レイアウト整理

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,11 @@
-import { StyleSheet } from "react-native";
-
 import { ActivityList } from "@/components/ActivityList";
 import DashboardScrollView from "@/components/AppScrollView";
 import { ConditionCard } from "@/components/ConditionCard";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { useListenActivities } from "@/hooks/useListenActivities";
 import { calcOverallScore } from "@/utils/scoring";
 import { useMemo } from "react";
+import { StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   const { activities } = useListenActivities();
@@ -39,23 +37,16 @@ export default function HomeScreen() {
 
   return (
     <DashboardScrollView>
-      <ThemedView>
-        <ConditionCard scoreLabel={overallScore.label} />
-        <ThemedView style={styles.space}>
-          <ThemedText style={styles.headingText}>Latest activities</ThemedText>
-          <ActivityList data={activities} style={styles.activityList} />
-        </ThemedView>
-      </ThemedView>
+      <ConditionCard scoreLabel={overallScore.label} />
+      <ThemedText style={styles.headingText}>Latest activities</ThemedText>
+      <ActivityList data={activities} style={styles.activityList} />
     </DashboardScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  space: {
-    paddingVertical: 16,
-  },
   headingText: {
-    marginTop: 16,
+    marginTop: 32,
     fontSize: 21,
     fontWeight: "600",
     color: "#333",
