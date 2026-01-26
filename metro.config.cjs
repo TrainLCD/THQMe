d8f0d9206ab821b6474c6d67f5c257eecbb098dd
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-01-26 15:51:52 +0900

    cjs

--- metro.config.cjs
+++ metro.config.cjs
@@ -5,6 +5,7 @@ const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
+  configPath: "./tailwind.config.cjs",
   // Force write CSS to file system instead of virtual modules
   // This fixes iOS styling issues in development mode
   forceWriteFileSystem: true,
